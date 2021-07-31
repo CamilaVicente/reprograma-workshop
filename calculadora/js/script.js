9b0d277fc2cc2b03aa2096714362a7cedc701b26
CamilaVicente <68302199+CamilaVicente@users.noreply.github.com>
2021-07-31 15:34:48 -0300

    primeiro Commit

---
+++ calculadora/js/script.js
@@ -0,0 +1,20 @@
+ console.log("Wello World!!")
+
+var inputSalario = document.querySelector('#ganho-mes')
+var inputHoras = document.querySelector('#horas-dia')
+var resultado = document.querySelector('span')
+
+
+
+function calcularValorHora(){
+
+    var salario = inputSalario.valueAsNumber
+    var horas = inputHoras.valueAsNumber
+    var horasMes = horas * 22
+    var valorHora = (salario / horasMes).toFixed(2)
+
+    resultado.textContent = "R$ " + valorHora   
+       
+   
+}
+
